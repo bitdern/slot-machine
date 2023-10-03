f1c26b4dfd84d5783e497e4ec6e66a793ce73d0a
bitdern <acnewell09@gmail.com>
2023-10-03 17:20:25 -0500

    added wheel spinning logic

--- project.js
+++ project.js
@@ -67,9 +67,31 @@ const getBet = (balance, lines) => {
 // 4. Spin slot machine
 const spin = () => {
   const symbols = [];
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
+
+  const reels = [];
+  for (let i = 0; i < COLS; i++) {
+    reels.push([]);
+    const reelSymbols = [...symbols];
+    for (let j = 0; j < ROWS; j++);
+    {
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+      const selectedSymbol = reelSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelSymbols.splice(randomIndex, 1);
+    }
+  }
+
+  return reels;
 };
 
 // General note: functions can be written anywhere *above* where they are invoked (below this line)
+const reels = spin();
+console.log(reels);
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
